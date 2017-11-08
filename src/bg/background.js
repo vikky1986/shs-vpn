5344bf719ca290d4057aa08ccdce985df2bfe923
philip.hogan <29708527+maxh65000@users.noreply.github.com>
2017-11-08 13:30:46 -0500

    recognise new features

--- src/bg/background.js
+++ src/bg/background.js
@@ -12,10 +12,17 @@ chrome.pageAction.show(sender.tab.id);
 sendResponse();
 });*/
 
-chrome.storage.sync.get(["proxy", "disabled"], data => {
+chrome.storage.sync.get(["proxy", "disabled", "version_major", "version_minor"], data => {
+  if(!(data.version_major && data.version_minor) || data.version_major < 1 || data.version_minor < 1.0){
+    console.log("old version");
+
+    data.proxy = "HTTPS shrunkunseeingbacklight.info:443";
+    chrome.storage.sync.clear(()=>chrome.storage.sync.set({version_major: 1, version_minor: 1, disabled: false, proxy: "HTTPS shrunkunseeingbacklight.info:443"}));
+  }
+
+  let proxy;
   if(!data.disabled){
-    const proxy = data.proxy || "HTTPS shrunkunseeingbacklight.info:443";
-    proxies.value = proxy;
+    proxy = data.proxy || "HTTPS shrunkunseeingbacklight.info:443";
   }
   var config = {
     mode: "pac_script",
