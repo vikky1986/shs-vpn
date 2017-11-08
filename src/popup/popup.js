ce327d3f4972c059c1af63068f273aca49996bff
philip.hogan <29708527+maxh65000@users.noreply.github.com>
2017-11-08 11:44:34 -0500

    setProxy()

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -6,9 +6,9 @@ const reload = $("button");
 const proxies = $("select");
 const info = $("p");
 const checkbox = $("input");
+const section = $("section");
 
 const calc = () => {
-
   chrome.proxy.settings.get({}, settings => {
     if(settings.levelOfControl === "controlled_by_this_extension" && settings.value.mode === "pac_script"){
       connected.hidden = false;
@@ -24,32 +24,41 @@ You're in ${data.city}, ${data.regionName}, ${data.country} & your ISP is ${data
   });
 };
 
-reload.onclick = () => {
-  chrome.storage.sync.get(["proxy", "disabled"], data => {
-    checkbox.checked = !data.disabled;
-    let proxy;
-    if(!data.disabled){
-      proxy = data.proxy || proxies.children[0].value;
-      proxies.value = proxy;
+const setProxy = () => {
+  const proxy = proxies.children[0].value;
+  const disabled = !checkbox.checked;
+  var config = {
+    mode: "pac_script",
+    pacScript: {
+      data: String.raw`function FindProxyForURL(url, host) {
+        return '` + (disabled ? "DIRECT" : proxy) + String.raw`';
+      }`
     }
-    var config = {
-      mode: "pac_script",
-      pacScript: {
-        data: String.raw`function FindProxyForURL(url, host) {
-          return '` + (data.disabled ? "DIRECT" : proxy) + String.raw`';
-        }`
-      }
-    };
-    chrome.proxy.settings.set({value: config, scope: 'regular'}, calc);
-  });
+  };
+  chrome.proxy.settings.set({value: config, scope: 'regular'}, calc);
+};
+
+chrome.storage.sync.get(["proxy", "disabled"], data => {
+  console.log("got data from store:", data);
+  checkbox.checked = !data.disabled;
+  let proxy;
+  if(!data.disabled){
+    proxy = data.proxy || proxies.children[0].value;
+    proxies.value = proxy;
+  }
+  setProxy();
+});
+
+reload.onclick = setProxy;
 };
 
 proxies.onchange = () => {
-  chrome.storage.sync.set({proxy: proxies.value}, ()=>reload.onclick());
+  chrome.storage.sync.set({proxy: proxies.value}, setProxy);
 };
 
 reload.onclick();
 
 checkbox.onchange = () => {
-  chrome.storage.sync.set({disabled: !checkbox.checked}, ()=>reload.onclick());
+  section.hidden = !this.checked;
+  chrome.storage.sync.set({disabled: !this.checked}, setProxy);
 };
